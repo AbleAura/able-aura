172aecdb6dc487be4142a92ff180e3f658eaaabc
emontes <emontes@turista.com.mx>
2021-12-27 21:05:21 -0500

    fixed height of top bar

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -21,7 +21,7 @@ const Wrapper = styled.div`
   z-index: 1;
   background-color: ${(props) => props.theme.colors.white2};
   width: 100%;
-  height: 109px;
+
   display: flex;
   flex-direction: column;
   align-items: center;
